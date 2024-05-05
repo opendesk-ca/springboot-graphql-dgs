e6c692f76f467ab1c32144ccbe3c169a1246f737
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-04 23:40:20 -0400

    SpringBoot GraphQL DGS project

---
+++ src/main/java/com/accounts/domain/TransactionType.java
@@ -0,0 +1,16 @@
+package com.accounts.domain;
+
+public enum TransactionType {
+    CHECK,
+    WITHDRAWAL,
+    TRANSFER,
+    POSDEBIT,
+    ATMWITHDRAWAL,
+    BILLPAYMENT,
+    FEE,
+    DEPOSIT,
+    ADJUSTMENT,
+    INTEREST,
+    DIVIDEND,
+    DIRECTDEPOSIT
+}

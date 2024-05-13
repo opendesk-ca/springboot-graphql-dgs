03df34cd6fbe822e61bdd2fe4b48a2701d539623
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-13 15:40:01 -0400

    Cleaning up implementation.

--- src/main/java/com/accounts/datafetcher/ClientDataFetcher.java
+++ src/main/java/com/accounts/datafetcher/ClientDataFetcher.java
@@ -21,6 +21,7 @@ public class ClientDataFetcher {
 
     @DgsQuery (field = "clients")
     public List<Client> getClients () {
+        log.info("Getting clients.");
         return clientService.getClients();
     }
 
@@ -41,7 +42,7 @@ public class ClientDataFetcher {
     public List<Client> clients (DgsDataFetchingEnvironment dfe){
         Account account = dfe.getSource();
 
-        log.info("Get Clients for Account ", account.getAccountId());
+        log.info("Get Clients for Account " + account.getAccountId());
 
         List<Client> clients = clientService.getClients(account.getAccountId());
         return clients;
@@ -50,14 +51,13 @@ public class ClientDataFetcher {
     @DgsEntityFetcher(name = "Account")
     public Account account (Map<String, Object> values) {
 
+        log.info("Fetching Account entities from Client Side : "  + values );
         Object accountId =  values.get("accountId");
 
         if (accountId instanceof Number) {
             return new Account(((Number) accountId).intValue(), null);
         } else if (accountId instanceof String) {
-            Account account = new Account(Integer.parseInt((String) accountId),
-                    clientService.getClients(Integer.parseInt((String) accountId)));
-            return account;
+            return new Account(Integer.parseInt((String) accountId),null);
         }else {
             throw new IllegalArgumentException("Object is not a Number");
         }

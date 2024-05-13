a35878011412328b34f2694a243b2eaaea5480be
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-13 15:55:53 -0400

    Clean up implementation. Adjust the log levels.

--- src/main/java/com/accounts/datafetcher/AccountsDataFetcher.java
+++ src/main/java/com/accounts/datafetcher/AccountsDataFetcher.java
@@ -3,14 +3,14 @@ package com.accounts.datafetcher;
 
 import com.accounts.entity.Account;
 import com.accounts.service.AccountService;
-
-import com.netflix.graphql.dgs.*;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 
 @DgsComponent
@@ -36,16 +36,5 @@ public class AccountsDataFetcher {
     public Boolean deleteAccount (@InputArgument("accountId") Integer accountId) {
         accountsService.deleteAccount (accountId);
         return Boolean.TRUE;
-    }
-
-    @DgsEntityFetcher(name = "Account")
-    public Account account (Map<String, Object> values) {
-
-        Optional<Account> account = accountsService.accounts()
-                .stream().filter(a->a.getAccountId()==Integer.parseInt((String)values.get("accountId")))
-                .findFirst();
-
-        if (account.isPresent()) return account.get();
-        return null;
     }
 }

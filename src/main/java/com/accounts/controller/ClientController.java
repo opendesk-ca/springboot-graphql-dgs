9191aaf023db3aa8830f8aeab1e554fbdd20dc2e
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-13 23:12:03 -0400

    Migrating Spring Controllers.

--- src/main/java/com/accounts/controller/ClientController.java
+++ src/main/java/com/accounts/controller/ClientController.java
@@ -1,5 +1,4 @@
-package com.accounts.datafetcher;
-
+package com.accounts.controller;
 
 import com.accounts.domain.Account;
 import com.accounts.domain.ClientInput;
@@ -8,16 +7,39 @@ import com.accounts.service.ClientService;
 import com.netflix.graphql.dgs.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Map;
 
-/*@DgsComponent
-@Slf4j*/
-public class ClientDataFetcher {
-/*
+@DgsComponent
+@Controller
+@Slf4j
+public class ClientController {
+
     @Autowired
     ClientService clientService;
+
+    @QueryMapping (name = "clients")
+    public List<Client> getClients () {
+        log.info("Getting clients.");
+        return clientService.getClients();
+    }
+
+    @MutationMapping
+    public Client addClient (@InputArgument("client") ClientInput client) {
+        log.info("Adding clients.");
+        return clientService.save (client);
+    }
+
+    @MutationMapping
+    public Boolean deleteClient (@InputArgument ("clientId") Integer clientId,
+                                 @InputArgument ("accountId") Integer accountId) {
+        log.info("Deleting clients.");
+        return clientService.delete (clientId, accountId);
+    }
 
     @DgsData(parentType = "Account", field = "client")
     public List<Client> clients (DgsDataFetchingEnvironment dfe){
@@ -42,6 +64,5 @@ public class ClientDataFetcher {
         }else {
             throw new IllegalArgumentException("Object is not a Number");
         }
-    }*/
+    }
 }
-

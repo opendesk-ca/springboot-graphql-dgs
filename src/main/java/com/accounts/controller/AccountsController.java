31497ad565b49a7f32e79ce3329375a15f320c82
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-19 12:30:36 -0400

    Added CRUD operations

--- src/main/java/com/accounts/controller/AccountsController.java
+++ src/main/java/com/accounts/controller/AccountsController.java
@@ -1,19 +1,23 @@
 package com.accounts.controller;
 
-import com.accounts.domain.BankAccount;
 import com.accounts.domain.Client;
+import com.accounts.entity.BankAccount;
 import com.accounts.service.BankService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @Slf4j
 public class AccountsController {
+
     @Autowired
     BankService bankService;
 
@@ -23,9 +27,34 @@ public class AccountsController {
         return bankService.getAccounts();
     }
 
-    @SchemaMapping (typeName = "BankAccount", field = "client")
-    Client getClient (BankAccount account) {
-        log.info("Getting client for " + account.getId());
-        return bankService.getClientByAccountId(account.getId());
+    @QueryMapping
+    BankAccount accountById (@Argument("accountId")  Long accountId){
+        log.info("Getting Account ");
+        return bankService.accountById(accountId);
+    }
+
+    @BatchMapping( field = "client", typeName = "BankAccountType" )
+    public Map<BankAccount, Client> clients(List<BankAccount> bankAccounts) {
+        log.info("Getting client for Accounts : " + bankAccounts.size());
+        return bankService.getBankAccountClientMap(bankAccounts);
+    }
+
+    @MutationMapping
+    Boolean addAccount (@Argument("account") BankAccount account)  {
+        log.info("Saving Account : " + account);
+        bankService.save(account);
+        return true;
+    }
+
+    @MutationMapping
+    BankAccount editAccount (@Argument("account") BankAccount account) {
+        log.info("Editing Account : " + account);
+        return bankService.modify(account);
+    }
+
+    @MutationMapping
+    Boolean deleteAccount (@Argument("id") Long accountId) {
+        log.info("Deleting Account : " + accountId);
+        return bankService.delete(accountId);
     }
 }
--- src/main/java/com/accounts/domain/Client.java
+++ src/main/java/com/accounts/domain/Client.java
@@ -2,12 +2,13 @@ package com.accounts.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Client {
-    private String id;
-    private String accountId;
+    private Long id;
     private String firstName;
     private String middleName;
     private String lastName;

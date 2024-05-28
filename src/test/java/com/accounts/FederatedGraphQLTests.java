1b551c1cc22fbecac375300aeba8163e05237446
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-27 20:16:50 -0400

    Added Federated Test Cases

--- src/test/java/com/accounts/FederatedGraphQLTests.java
+++ src/test/java/com/accounts/FederatedGraphQLTests.java
@@ -39,29 +39,28 @@ public class FederatedGraphQLTests {
     @Order(1)
     @Sql(scripts = "/sql/drop-and-create-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void testAddAccountMutation() {
-        String mutation = """
-                    mutation {
-                        addAccount(account: {
-                            accountId: "12345",
-                            accountType: CHECKING,
-                            accountNumber: "CHK987654321",
-                            currency: "USD",
-                            balance: 2500.00,
-                            status: OPEN,
-                            lastActivityDate: "2024-05-10T14:48:00.000Z"
-                        }) {
-                            accountId
-                            accountType
-                            accountNumber
-                            currency
-                            balance
-                            status
-                            lastActivityDate
-                        }
-                    }
-        """;
 
-        String graphqlPayload = String.format("{\"query\":\"%s\"}", mutation.replace("\"", "\\\"").replace("\n", ""));
+        String graphqlPayload = getGraphqlPayload("""
+                            mutation {
+                                addAccount(account: {
+                                    accountId: "12345",
+                                    accountType: CHECKING,
+                                    accountNumber: "CHK987654321",
+                                    currency: "USD",
+                                    balance: 2500.00,
+                                    status: OPEN,
+                                    lastActivityDate: "2024-05-10T14:48:00.000Z"
+                                }) {
+                                    accountId
+                                    accountType
+                                    accountNumber
+                                    currency
+                                    balance
+                                    status
+                                    lastActivityDate
+                                }
+                            }
+                """);
         HttpEntity<String> request = new HttpEntity<>(graphqlPayload, headers);
 
         URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:4000/graphql").build().toUri();
@@ -74,34 +73,70 @@ public class FederatedGraphQLTests {
 
     @Test
     @Order(2)
-    @Sql(scripts = "/sql/cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void testFederatedQuery() {
-        String query = """
-            {
-              accounts {
-                accountId
-                client {
-                  clientId
-                  firstName
-                  lastName
-                }
-                accountNumber
-                accountType
-                balance
-                currency
-                lastActivityDate
-                status
-              }
-            }
-        """;
+    void testAddClientMutation() {
 
-        String graphqlPayload = String.format("{\"query\":\"%s\"}", query.replace("\"", "\\\"").replace("\n", ""));
+        String graphqlPayload = getGraphqlPayload("""
+                        mutation {
+                            addClient(client: {
+                                clientId: 5000,
+                                accountId: 12345,
+                                firstName: "John",
+                                middleName: "Quincy",
+                                lastName: "Doe",
+                                country: "US"
+                            }) {
+                                clientId
+                                accountId
+                                firstName
+                                middleName
+                                lastName
+                                country
+                            }
+                        }
+                """);
         HttpEntity<String> request = new HttpEntity<>(graphqlPayload, headers);
 
-        ResponseEntity<String> response = restTemplate.exchange("/graphql", HttpMethod.POST, request, String.class);
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:4000/graphql").build().toUri();
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
 
         // Assert the response
         assertEquals(200, response.getStatusCodeValue());
         // Additional assertions on response body
+    }
+
+
+    @Test
+    @Order(3)
+    @Sql(scripts = "/sql/cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void testFederatedQuery() {
+        String graphqlPayload = getGraphqlPayload("""
+                    {
+                      accounts {
+                        accountId
+                        client {
+                          clientId
+                          firstName
+                          lastName
+                        }
+                        accountNumber
+                        accountType
+                        balance
+                        currency
+                        lastActivityDate
+                        status
+                      }
+                    }
+                """);
+        HttpEntity<String> request = new HttpEntity<>(graphqlPayload, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("/graphql", HttpMethod.POST, request, String.class);
+        System.out.println(response);
+        // Assert the response
+        assertEquals(200, response.getStatusCodeValue());
+        // Additional assertions on response body
+    }
+
+    private static String getGraphqlPayload(String query) {
+        return String.format("{\"query\":\"%s\"}", query.replace("\"", "\\\"").replace("\n", ""));
     }
 }

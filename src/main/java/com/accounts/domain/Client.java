de583de3b2d08626cbe4161aab135a078b1c295f
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-05-28 23:21:39 -0400

    JDK Upgrade to 21

--- src/main/java/com/accounts/domain/Client.java
+++ src/main/java/com/accounts/domain/Client.java
@@ -1,16 +1,3 @@
 package com.accounts.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class Client {
-    private Long id;
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String country;
-}
+public record Client(Long id, String firstName, String middleName, String lastName, String country) {}

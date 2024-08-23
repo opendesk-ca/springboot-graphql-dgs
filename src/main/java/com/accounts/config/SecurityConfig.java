39ecf8a5524541c24d600156858305c611d4506d
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-08-23 00:27:00 -0400

    JWT Authentication

--- src/main/java/com/accounts/config/SecurityConfig.java
+++ src/main/java/com/accounts/config/SecurityConfig.java
@@ -3,25 +3,8 @@ package com.accounts.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
@@ -39,35 +22,5 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);  // Disable CSRF protection
 
         return http.build();
-    }
-
-    @Bean
-    public WebClient webClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
-        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
-                new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        return WebClient.builder()
-                .filter(oauth2)
-                .build();
-    }
-
-    @Bean
-    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
-            ReactiveClientRegistrationRepository clientRegistrationRepository,
-            ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
-
-        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
-                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
-                        .authorizationCode()
-                        .refreshToken()
-                        .clientCredentials()
-                        .password()
-                        .build();
-
-        DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
-                new DefaultReactiveOAuth2AuthorizedClientManager(
-                        clientRegistrationRepository, authorizedClientRepository);
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-
-        return authorizedClientManager;
     }
 }
--- src/main/java/com/accounts/controller/AccountsController.java
+++ src/main/java/com/accounts/controller/AccountsController.java
@@ -3,10 +3,15 @@ package com.accounts.controller;
 import com.accounts.domain.BankAccount;
 import com.accounts.domain.Client;
 import com.accounts.service.BankService;
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,23 +28,27 @@ public class AccountsController {
     BankService bankService;
 
     @QueryMapping
-    public Mono<List<BankAccount>> accounts() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(context -> {
-                    Authentication authentication = context.getAuthentication();
-                    log.info("Getting Accounts for user: " + authentication.getName());
-                    return bankService.getAccounts();
-                });
+    public List<BankAccount> accounts() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("Getting Accounts for user: " + authentication.getName());
+        return bankService.getAccounts();
     }
 
     @SchemaMapping(typeName = "BankAccount", field = "client")
-    public Mono<Client> getClient(BankAccount account) {
-        return ReactiveSecurityContextHolder.getContext()
-                .flatMap(context -> {
-                    Authentication authentication = context.getAuthentication();
-                    log.info("Getting client for " + account.id() + " for user: " + authentication.getName());
-                    return Mono.fromCallable(() -> bankService.getClientByAccountId(account.id()))
-                            .subscribeOn(Schedulers.boundedElastic());
-                });
+    public Client getClient(BankAccount account) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("Getting client for " + account.id() + " for user: " + authentication.getName());
+        return bankService.getClientByAccountId(account.id());
+    }
+
+    @GraphQlExceptionHandler
+    public GraphQLError handle(@NonNull Exception ex, @NonNull DataFetchingEnvironment environment) {
+        return GraphQLError
+                .newError()
+                .errorType(ErrorType.BAD_REQUEST)
+                .message(ex.getMessage())
+                .path(environment.getExecutionStepInfo().getPath())
+                .location(environment.getField().getSourceLocation())
+                .build();
     }
 }

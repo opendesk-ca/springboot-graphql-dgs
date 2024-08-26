cce3145e256625f02d71ed7b917511818fee7c2e
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-08-25 23:10:06 -0400

    HTTP Authentication Code

--- src/main/java/com/accounts/controller/AccountsController.java
+++ src/main/java/com/accounts/controller/AccountsController.java
@@ -7,12 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -23,23 +18,14 @@ public class AccountsController {
     BankService bankService;
 
     @QueryMapping
-    public Mono<List<BankAccount>> accounts() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(context -> {
-                    Authentication authentication = context.getAuthentication();
-                    log.info("Getting Accounts for user: " + authentication.getName());
-                    return bankService.getAccounts();
-                });
+    List<BankAccount> accounts (){
+        log.info("Getting Accounts ");
+        return bankService.getAccounts();
     }
 
-    @SchemaMapping(typeName = "BankAccount", field = "client")
-    public Mono<Client> getClient(BankAccount account) {
-        return ReactiveSecurityContextHolder.getContext()
-                .flatMap(context -> {
-                    Authentication authentication = context.getAuthentication();
-                    log.info("Getting client for " + account.id() + " for user: " + authentication.getName());
-                    return Mono.fromCallable(() -> bankService.getClientByAccountId(account.id()))
-                            .subscribeOn(Schedulers.boundedElastic());
-                });
+    @SchemaMapping (typeName = "BankAccount", field = "client")
+    Client getClient (BankAccount account) {
+        log.info("Getting client for " + account.id());
+        return bankService.getClientByAccountId(account.id());
     }
 }

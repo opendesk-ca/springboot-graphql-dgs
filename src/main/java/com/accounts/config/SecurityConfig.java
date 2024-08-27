dc6926516c1408a2a843f33ab8c463d19eedaf50
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-08-27 01:16:34 -0400

    Coding standard - changes.

--- src/main/java/com/accounts/config/SecurityConfig.java
+++ src/main/java/com/accounts/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri")
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String jwtIssuerUri;
 
     @Bean
